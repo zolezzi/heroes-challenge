0fde8353b3f8a08954f795aa6428dec14cd52e74
zolezzi <cezcardozo23@gmail.com>
2023-01-26 14:07:48 -0300

    adding project skeleton

---
+++ src/main/java/com/es/mindata/heroeschallenge/HeroesChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.es.mindata.heroeschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es/mindata/heroeschallenge/HeroesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.mindata.heroeschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
